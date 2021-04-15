00e35778b33c92841180bfdcc7e122f9591d0733
Zhuoyang Han <81492838+hzy625@users.noreply.github.com>
2021-04-15 13:43:17 -0400

    Update views.py

--- squirrel/views.py
+++ squirrel/views.py
@@ -22,3 +22,36 @@ def sightings_list(request):
     table.paginate(page=request.GET.get("page", 1), per_page=50)
     return render(request, 'squirrel/list.html', {'table': table})
 
+def update(request, unique_squirrel_id):
+    """
+    update a row data
+    """
+    data = get_object_or_404(Squirrel, unique_squirrel_id=unique_squirrel_id)
+    if request.method == 'GET':
+        user_form = SquirrelUpdateForm(instance=data) # 生成form
+        return render(request, 'squirrel/edit.html', {'user_form': user_form})
+    else:
+        user_form = SquirrelUpdateForm(request.POST, instance=data) # 把Post中的数据添加到form中
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'update success')
+            return redirect('sightings') # 返回首页
+        else:
+            return render(request, 'squirrel/edit.html', {'user_form': user_form})   
+
+def add(request):
+    """
+    upload
+    """
+    if request.method == 'GET':
+        user_form = SquirrelForm() # 生成form
+        return render(request, 'squirrel/add.html', {'user_form': user_form})
+    else:
+        user_form = SquirrelForm(request.POST) # 把Post中的数据添加到form中
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'add success')
+            return redirect('sightings') # TODO
+        else:
+            return render(request, 'squirrel/add.html', {'user_form': user_form})    
+
